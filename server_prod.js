f090c1f3a13c106792c55f58e6d086285dbc64e0
Ethan Pollitt <ethanpollitt@gmail.com>
2017-02-07 17:44:09 -0500

    Update prod server config

--- server_prod.js
+++ server_prod.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var router = express.Router();
 
 var path = __dirname + '/';
 
@@ -9,10 +8,10 @@ var compression = require("compression");
 app.use(compression());
 
 var routing = require("./node/routing.js");
-routing.set(app, router, path)
+routing.set(app, path)
 
 var apis = require("./node/api.js")
-apis.set(app, router, path);
+apis.set(app, path);
 
 app.use("*", function(req,res){
   res.sendFile(path + "404.html");
